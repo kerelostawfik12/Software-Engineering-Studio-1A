fc8fd6732186ab1a6b35ec33a9ea74c49de6c238
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2019-08-04 19:33:56 +1000

    Tested adding some new components

--- Studio1BTask/Controllers/SampleDataController.cs
+++ Studio1BTask/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace Studio1BTask.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Queensland"
         };
 
         [HttpGet("[action]")]
