4bb24f21b588cfb2387a0a933088b66f3cbfeeb8
haider iftikhar <mohammad.h.iftihar@student.uts.edu.au>
2019-08-21 17:11:01 +1000

    created cart-item

--- Studio1BTask/ClientApp/src/app/cart/cart.component.ts
+++ Studio1BTask/ClientApp/src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  public items : Item[];
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+
+    http.get<Item[]>(baseUrl + 'api/Data/AllItems').subscribe(result => {
+      this.items = result;
+    }, error => console.error(error));
+  }
 
   ngOnInit() {
 
