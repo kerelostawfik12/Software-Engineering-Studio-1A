893163d4c8154cefc2180d9005529c8f0f7b17b5
Helena <helena.e.tran@student.uts.edu.au>
2019-08-28 16:57:41 +1000

    Should now get connection string from Azure DevOps when deployed

---
+++ Studio1BTask/ClientApp/src/app/new-item-form-component/new-item-form-component.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-new-item-form-component',
+  templateUrl: './new-item-form-component.component.html',
+  styleUrls: ['./new-item-form-component.component.css']
+})
+export class NewItemFormComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
