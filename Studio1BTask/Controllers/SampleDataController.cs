5b15b03aeb8ed349f25d090154f532b85bcd50ab
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2019-09-10 21:01:07 +1000

    Fixed compile error

--- Studio1BTask/Controllers/SampleDataController.cs
+++ Studio1BTask/Controllers/SampleDataController.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Studio1BTask.Models;
 
 namespace Studio1BTask.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
+        private static readonly string[] Summaries =
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Queensland", "Hell"
         };
@@ -17,8 +16,6 @@ namespace Studio1BTask.Controllers
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
-            TestDatabase();
-            
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -28,36 +25,13 @@ namespace Studio1BTask.Controllers
             });
         }
 
-        public void TestDatabase()
-        {
-            using (var context = new DbContext())
-            {
-                var newEntity = new TestModel()
-                {
-                    AString = "hii", ANumber = 393
-                };
-                context.TestModels.Add(newEntity);
-
-                context.ForeignKeyTests.Add(new ForeignKeyTest()
-                {
-                    TestModel = newEntity
-                });
-                
-                context.SaveChanges();
-            }
-            
-        }
-
         public class WeatherForecast
         {
             public string DateFormatted { get; set; }
             public int TemperatureC { get; set; }
             public string Summary { get; set; }
 
-            public int TemperatureF
-            {
-                get { return 32 + (int) (TemperatureC / 0.5556); }
-            }
+            public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
         }
     }
 }
