09d35d6dc32d673c65bb82061fee4893853e77d5
pseudoptics <bananamagic.com@gmail.com>
2019-09-25 16:42:31 +1000

    Removed markdown support due to markdown package causing errors.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MarkdownModule} from 'ngx-markdown';
 
 import {AppComponent} from './app.component';
 import {NavMenuComponent} from './nav-menu/nav-menu.component';
@@ -47,7 +46,6 @@ import {AdminComponent} from './admin/admin.component';
     AdminComponent
   ],
   imports: [
-    MarkdownModule.forRoot(),
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
