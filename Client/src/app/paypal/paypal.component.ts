66bc6f7cf3422b15fe8b643aa01819197a26b778
Haider Iftikhar <haiderezio121@gmail.com>
2019-10-08 23:53:02 +1100

    added Basic Payment, shipping and handling, recurring payment(HTML) to test zone.component. created the paypal API(Sandbox Testing)

--- Client/src/app/paypal/paypal.component.ts
+++ Client/src/app/paypal/paypal.component.ts
@@ -12,8 +12,16 @@ export class PaypalComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    paypal.Buttons().render('#buttons');
+    paypal.Buttons().render('#buttons',);
+
+    // paypal.configure({
+    //  'mode' : 'Sandbox', //live or sandbox
+    //  'client_id' : 'AbY5qhTQK2zo_163bAA1xkA19YkBMg8uiLqqh3MIAULsH2yF7T2ZGQ38cx740JmcUq24_bwNLS6-e3LF',
+    //  'client_secret' : 'EMicuENMd4PD-W86tF2wEvUFHTCf_bHhNqJYwc7zsRvEnKKj1tmSDlv30H220hxyeq-f0rbevVAJgGrk'
+
+
   }
+
 
 
 
